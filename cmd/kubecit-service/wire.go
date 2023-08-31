90f53c40bff9e1c998d9c6571ab061af12fc723d
mouuii <49775493+mouuii@users.noreply.github.com>
2023-08-31 17:54:50 +0800

    first init

---
+++ cmd/kubecit-service/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"kubecit-service/internal/biz"
+	"kubecit-service/internal/conf"
+	"kubecit-service/internal/data"
+	"kubecit-service/internal/server"
+	"kubecit-service/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
