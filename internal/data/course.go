2fa44dc3ee43dc2bb720b73347088628e0cbd48d
Boyce <zhong.zhao@viatris.com>
2023-09-09 11:21:03 +0800

    最新课程

--- internal/data/course.go
+++ internal/data/course.go
@@ -59,6 +59,7 @@ func (c courseRepo) SearchCourse(ctx context.Context, pageNum, pageSize int, cat
 }
 
 func (c courseRepo) MostNewCourse(ctx context.Context) ([]*biz.Course, error) {
+	//倒序取20条
 	courses, err := c.data.db.Course.Query().Limit(20).Order(ent.Desc(course.FieldCreatedAt)).All(ctx)
 	if err != nil {
 		return nil, err
